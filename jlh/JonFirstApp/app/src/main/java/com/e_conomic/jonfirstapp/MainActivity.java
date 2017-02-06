0b393257527676f2442e4ebead0c27e5a296bcfe
Jon Holmberg <jon.holmberg@visma.com>
2017-02-06 16:31:53 +0100

    Added sendMessage method and created first Intent

--- jlh/JonFirstApp/app/src/main/java/com/e_conomic/jonfirstapp/MainActivity.java
+++ jlh/JonFirstApp/app/src/main/java/com/e_conomic/jonfirstapp/MainActivity.java
@@ -1,13 +1,33 @@
 package com.e_conomic.jonfirstapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Key for the intent extra holding the message.
+    public final static String EXTRA_MESSAGE = "com.e-conomic.jonfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        // Create intent.
+        Intent intent = new Intent(this, DisplayMessageActivity);
+
+        // Add (user) entered message to the created intent.
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+
+        // Start an instance of the DisplayMessageActivity specified by the intent.
+        startActivity(intent);
     }
 }
