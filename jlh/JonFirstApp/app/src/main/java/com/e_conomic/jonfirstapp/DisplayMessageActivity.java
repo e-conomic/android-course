fc4f4c3f07363ceefdb0df9e08fab2a44fd3bdf5
Jon Holmberg <jon.holmberg@visma.com>
2017-02-08 13:03:33 +0100

    Created TextView in xml and added corresponding reference in java

--- jlh/JonFirstApp/app/src/main/java/com/e_conomic/jonfirstapp/DisplayMessageActivity.java
+++ jlh/JonFirstApp/app/src/main/java/com/e_conomic/jonfirstapp/DisplayMessageActivity.java
@@ -3,7 +3,6 @@ package com.e_conomic.jonfirstapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
@@ -17,13 +16,8 @@ public class DisplayMessageActivity extends AppCompatActivity {
         Intent intent = getIntent();
         // Get the message to be shown.
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-        // Create the text view and set the message.
-        TextView textView = new TextView(this);
-        textView.setTextSize(40);
+        // Get the text view and set the message.
+        TextView textView = (TextView) findViewById(R.id.display_message);
         textView.setText(message);
-
-        // Add the text view to the corresponding layout to show the message.
-        ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display_message);
-        layout.addView(textView);
     }
 }
