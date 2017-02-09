5f108949801b5e744875e69d37fa7feddf8db651
Jon Holmberg <jon.holmberg@visma.com>
2017-02-09 13:22:11 +0100

    Added Fragments

--- jlh/JonFirstApp/app/src/main/java/com/e_conomic/jonfirstapp/MainActivity.java
+++ jlh/JonFirstApp/app/src/main/java/com/e_conomic/jonfirstapp/MainActivity.java
@@ -5,11 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.support.v4.app.FragmentActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     // Key for the intent extra holding the message.
-    public final static String EXTRA_MESSAGE = "com.e_conomic.jonfirstapp.MESSAGE";
+    public final static String EXTRA_MESSAGE = "MESSAGE";
 
     // Used to hold the reference to the text field containing the message to be written.
     private EditText editText = null;
@@ -17,11 +18,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        editText = (EditText) findViewById(R.id.edit_message);
+        setContentView(R.layout.fragment_layout);
+        //editText = (EditText) findViewById(R.id.edit_message);
     }
 
-    /** Called when the user clicks the Send button */
+    /** Called when the user clicks the Send button
     public void sendMessage(View view) {
         // Create intent.
         Intent intent = new Intent(this, DisplayMessageActivity.class);
@@ -32,5 +33,5 @@ public class MainActivity extends AppCompatActivity {
 
         // Start an instance of the DisplayMessageActivity specified by the intent.
         startActivity(intent);
-    }
+    }*/
 }
