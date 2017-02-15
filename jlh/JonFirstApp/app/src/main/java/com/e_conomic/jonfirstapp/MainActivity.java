b908800069ae88e893da6f4500be4d370171b2dc
Jon Holmberg <jon.holmberg@visma.com>
2017-02-15 11:00:50 +0100

    The fragment that shows the message now pops up dynamically

--- jlh/JonFirstApp/app/src/main/java/com/e_conomic/jonfirstapp/MainActivity.java
+++ jlh/JonFirstApp/app/src/main/java/com/e_conomic/jonfirstapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.e_conomic.jonfirstapp;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
@@ -7,6 +8,8 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -25,12 +28,15 @@ public class MainActivity extends FragmentActivity {
     // Message string
     private String message = "";
 
+    // Show/hide button
+    private Button button_showhide;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_layout);
         editText = (EditText) findViewById(R.id.edit_message);
-
+        button_showhide = (Button) findViewById(R.id.button_showhide);
     }
 
     /** Called when the user clicks the Send button */
@@ -68,29 +74,52 @@ public class MainActivity extends FragmentActivity {
     public void showHide(View view) {
 
         FragmentManager fragmentManager = getSupportFragmentManager();
+        View fragment_container = findViewById(R.id.land_fragment_container);
 
         // If the displaying fragment has not been initialized.
-        if (display_message_fragment == null
-                && findViewById(R.id.land_fragment_container) != null) {
+        if (display_message_fragment == null && fragment_container != null) {
             // Create and add Fragment
             display_message_fragment = new DisplayMessageFragment();
             fragmentManager.beginTransaction()
                     .add(R.id.land_fragment_container, display_message_fragment).commitNow();
+            button_showhide.setText(R.string.button_hidemsg);
+
+            // Set width of fragment layout.
+            changeFragmentLayout(fragment_container, ViewGroup.LayoutParams.MATCH_PARENT);
             return;
         }
 
         // Hide Fragment if it is visible.
         if (display_message_fragment.isVisible()) {
             fragmentManager.beginTransaction().hide(display_message_fragment).commitNow();
+            button_showhide.setText(R.string.button_showmsg);
+
+            // Set width of fragment layout.
+            changeFragmentLayout(fragment_container, ViewGroup.LayoutParams.WRAP_CONTENT);
             return;
         }
 
         // Show Fragment if it is hidden.
         if (display_message_fragment.isHidden()) {
             fragmentManager.beginTransaction().show(display_message_fragment).commitNow();
+            button_showhide.setText(R.string.button_hidemsg);
+
+            // Set width of fragment layout.
+            changeFragmentLayout(fragment_container, ViewGroup.LayoutParams.MATCH_PARENT);
             return;
         }
 
+    }
+
+    /** Helper function to change the layout of the fragment (to hide and show it on screen) */
+    private void changeFragmentLayout(View fragment_layout, int newLayoutParam) {
+        ViewGroup.LayoutParams layoutParams = fragment_layout.getLayoutParams();
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            layoutParams.height = newLayoutParam;
+        } else {
+            layoutParams.width = newLayoutParam;
+        }
+        fragment_layout.setLayoutParams(layoutParams);
     }
 
     /** Called when the user clicks the clear messages button */
