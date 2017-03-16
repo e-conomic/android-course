b663d266ede327d4299171fae26c09b8465a0f50
Jon Holmberg <jon.holmberg@visma.com>
2017-03-16 15:24:18 +0100

    Chapter 14: Visibility modifiers

--- jlh/WeatherApp/app/src/main/java/com/e_conomic/weatherapp/domain/model/DomainClasses.kt
+++ jlh/WeatherApp/app/src/main/java/com/e_conomic/weatherapp/domain/model/DomainClasses.kt
@@ -1,8 +1,9 @@
 package com.e_conomic.weatherapp.domain.model
 
 data class ForecastList(val city: String, val country: String, val dailyForecast: List<Forecast>) {
-    operator fun get(position: Int): Forecast = dailyForecast[position]
-    fun size(): Int = dailyForecast.size
+    operator fun get(position: Int) = dailyForecast[position]
+    val size: Int
+        get() = dailyForecast.size
 }
 data class Forecast(val date: String, val description: String, val high: Int, val low: Int,
                     val iconUrl: String)
