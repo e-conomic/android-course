34df38cf2d6194cbc1f3e1f650bb78a470b7fa38
Denys Meloshyn <dme@wallmob.com>
2017-04-06 12:34:29 +0200

    T04a.01 - add child activity

---
+++ denys/Exercises/T04a.01-Exercise-AddNewActivity/app/src/main/java/com/example/android/explicitintent/ChildActivity.java
@@ -0,0 +1,17 @@
+package com.example.android.explicitintent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ChildActivity extends AppCompatActivity {
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child);
+
+        textView = (TextView) findViewById(R.id.tv_display);
+    }
+}
